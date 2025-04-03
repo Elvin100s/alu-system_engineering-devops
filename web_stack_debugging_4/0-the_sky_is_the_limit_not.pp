2a099738f0b66ae552009c70d229d95fb359dc53
Elvin <e.cyubahiro@alustudent.com>
2025-04-03 22:40:45 +0200

    Update 0-the_sky_is_the_limit_not.pp

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,21 +1,36 @@
-# This Puppet script increases the ULIMIT of nginx default file to handle more concurrent connections
-# It modifies both the soft and hard limits for the nginx process
-
-# Increase ULIMIT in nginx default configuration
-exec { 'increase-ulimit':
-  command => '/bin/sed -i \'s/ULIMIT="-n 15"/ULIMIT="-n 4096"/\' /etc/default/nginx',
-  notify  => Exec['set-hard-limit'],
+# 1. Ensure nginx package is installed
+package { 'nginx':
+  ensure => installed,
 }
 
-# Increase hard limit for nginx processes
-exec { 'set-hard-limit':
-  command     => '/bin/sed -i \'1s/^/nginx\tsoft\tnofile\t4096\nnginx\thard\tnofile\t4096\n/\' /etc/security/limits.conf',
-  refreshonly => true,
-  notify      => Exec['restart-nginx'],
+# 2. Modify ULIMIT in nginx defaults (using more robust regex)
+exec { 'increase-nginx-ulimit':
+  command => "/bin/sed -i 's/^ULIMIT=.*/ULIMIT=\"-n 4096\"/' /etc/default/nginx",
+  unless  => "grep -q '^ULIMIT=\"-n 4096\"' /etc/default/nginx",
+  require => Package['nginx'],
+  notify  => Exec['set-system-limits'],
 }
 
-# Restart nginx to apply changes
-exec { 'restart-nginx':
-  command     => '/usr/sbin/service nginx restart',
+# 3. Set system-wide limits (idempotent)
+exec { 'set-system-limits':
+  command     => "/bin/echo -e 'nginx soft nofile 4096\nnginx hard nofile 4096\n' >> /etc/security/limits.conf",
+  unless      => "grep -q 'nginx.*nofile 4096' /etc/security/limits.conf",
   refreshonly => true,
+  notify      => Service['nginx'],
+}
+
+# 4. Configure nginx worker connections
+file_line { 'increase-worker-connections':
+  path    => '/etc/nginx/nginx.conf',
+  line    => 'worker_connections 4096;',
+  match   => '^worker_connections',
+  notify  => Service['nginx'],
+}
+
+# 5. Ensure nginx service
+service { 'nginx':
+  ensure     => running,
+  enable     => true,
+  hasrestart => true,
+  require    => Package['nginx'],
 }
